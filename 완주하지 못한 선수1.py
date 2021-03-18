57ec8f725fb2a507e918f845bffaa8721e32a03c
ssowon <thdnjs5018@naver.com>
2021-03-19 04:13:12 +0900

    완주하지 못한 선수

---
+++ 완주하지 못한 선수1.py
@@ -0,0 +1,6 @@
+#효율성 검사 실패
+
+def solution(participant, completion):
+    for i in completion : participant.remove(i)
+    answer = participant[0]
+    return answer
---
+++ 완주하지 못한 선수2.py
@@ -0,0 +1,12 @@
+#효율성 검사까지 모두 통과
+
+def solution(participant, completion):
+    answer = ''
+
+    participant.sort()
+    completion.sort()
+    for i in range(len(participant)):
+        if participant[i] != completion[i] :
+            answer = participant[i]
+            break
+    return answer
