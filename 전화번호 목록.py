00d5bef2280d6821c542f3618b8b38d561d56933
ssowon <thdnjs5018@naver.com>
2021-03-19 04:59:20 +0900

    전화번호 목록

---
+++ 전화번호 목록.py
@@ -0,0 +1,13 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1) :
+        if phone_book[i][0] is not phone_book[i+1][0]:
+            continue
+        if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]  :
+            return False
+    return answer
+
+
+phone_book = ["123","456","789"]
+print(solution(phone_book))
