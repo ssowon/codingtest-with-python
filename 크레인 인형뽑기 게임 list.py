7d81474374ff9b2ac6924bcfa43b4b925752b181
ssowon <thdnjs5018@naver.com>
2021-04-01 19:46:18 +0900

    크레인 인형뽑기 게임

---
+++ 크레인 인형뽑기 게임 list.py
@@ -0,0 +1,24 @@
+from collections import deque
+def solution(board, moves):
+    answer = 0
+
+    basket = deque()
+    depth = len(board)
+
+    for i in moves :
+        for j in range(depth) :
+            if board[j][i-1] > 0 :
+                toy = board[j][i-1]
+                board[j][i-1] = 0
+                if len(basket) > 0 and basket[-1] == toy:
+                    basket.pop()
+                    answer += 2
+                    break
+
+                else :
+                    basket.append(toy)
+                    break
+
+    return answer
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4]))
