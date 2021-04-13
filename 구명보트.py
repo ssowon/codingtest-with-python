9d8284e6a768866e49e4a45bd1700629485242fc
ssowon <thdnjs5018@naver.com>
2021-04-13 23:23:47 +0900

    구명보트

---
+++ 구명보트.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    print(people)
+    for i in range(len(people)):
+
+    return answer
+
+print(solution([70, 50, 80, 50], 100)) #3
+print(solution([70, 80, 50], 100)) #3
