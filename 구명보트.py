106a41638f6fbaa6c97f4b6100fbcde2bd8f1cf5
ssowon <thdnjs5018@naver.com>
2021-04-13 23:44:58 +0900

    구명보트 해결

--- 구명보트.py
+++ 구명보트.py
@@ -2,7 +2,22 @@ def solution(people, limit):
     answer = 0
     people.sort()
     print(people)
+    start = 0
+    last = len(people)-1
     for i in range(len(people)):
+        if start == last :
+            answer += 1
+            break
+        elif start > last :
+            break
+        if people[start]+people[last] > limit:
+            last -= 1
+            answer += 1
+        else :
+            start += 1
+            last -= 1
+            answer += 1
+
 
     return answer
 
