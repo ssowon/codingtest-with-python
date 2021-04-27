455908593ce7977a8c3604fa7cd6cf8ba38736d0
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2021-04-27 15:02:48 +0900

    Update 위장.py

--- 위장.py
+++ 위장.py
@@ -5,6 +5,6 @@ def solution(clothes):
     cnt = Counter([kind for name, kind in clothes])
     print(cnt)
     answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
-    return answer
+    return answer 
 
 print(solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
